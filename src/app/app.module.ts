e9b69c20ef2ac99338e679a5048533c8cd3c600c
André Gomes <1201108@isep.ipp.pt>
2023-09-18 14:59:06 +0100

    Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { LoginComponent } from './login/login.component';
-import { TransactionsComponent } from './transactions/transactions.component';
-import { FundsComponent } from './funds/funds.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import {NgOptimizedImage} from "@angular/common";
+import {MaterialModule} from './material/material.module';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {SidebarComponent} from './components/sidebar/sidebar.component';
+import {LoginComponent} from './login/login.component';
+import {TransactionsComponent} from './transactions/transactions.component';
+import {FundsComponent} from './funds/funds.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
@@ -26,9 +30,12 @@ import {NgOptimizedImage} from "@angular/common";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgOptimizedImage
+    NgOptimizedImage,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
